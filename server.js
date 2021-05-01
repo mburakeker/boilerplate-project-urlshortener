e6a94b264f14521415ed7db3060ded8d6c0fea97
Burak Eker <mehmetburakeker@gmail.com>
2021-05-01 12:23:38 +0300

    remove log mongo_uri

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const { Schema } = mongoose;
